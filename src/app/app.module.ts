66d5040658b9ff5cad4c846e25f73e482ec16f40
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-05-14 16:47:26 +0100

    single orders page ui via routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
+import { CustomersModule } from './customers/customers.module';
+import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CustomersModule,
-    SharedModule,
-    CoreModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, CustomersModule, OrdersModule, SharedModule, CoreModule, AppRoutingModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
